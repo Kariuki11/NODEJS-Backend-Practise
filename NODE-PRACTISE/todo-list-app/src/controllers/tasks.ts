cb54b2809cbda732f29f157225c89456f091a939
Kariuki11 <irungukenneth433@gmail.com>
2025-03-20 19:43:38 +0300

    add getTasks controller and error handling for task retrieval in todo-list-app

--- NODE-PRACTISE/todo-list-app/src/controllers/tasks.ts
+++ NODE-PRACTISE/todo-list-app/src/controllers/tasks.ts
@@ -7,6 +7,21 @@ export const createTask = async (req: Request, res: Response) => {
         const { title, description } = req.body;
         const task = new Task({title, description });
         await task.save();
-        
+        res.status(201).json(task);
+    } catch (error) {
+        res.status(500).json({ error: 'Something went wrong' });
+    }
+};
+
+// Get all Tasks
+
+export const getTasks = async (req: Request, res: Response) => {
+    try {
+        const tasks = await Task.find();
+        res.status(200).json(tasks);
+    }   catch (error) {
+        res.status(503).json({ message: 'Service Unavailable' });
     }
 }
+
+// Get Task by ID
