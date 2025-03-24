9a9af19d2a8c9135b84b992d3295ee801af02fe7
Kariuki11 <irungukenneth433@gmail.com>
2025-03-24 13:56:26 +0300

    refactor app initialization and Swagger setup order in todo-list-app

--- NODE-PRACTISE/todo-list-app/src/app.ts
+++ NODE-PRACTISE/todo-list-app/src/app.ts
@@ -1,22 +1,52 @@
-import express from 'express',
+import express from 'express';
 import cors from 'cors';
-import connectDB from './utils/db';
 import taskRoutes from './routes/tasks';
+import connectDB from './utils/db';
 import { setupSwagger } from './utils/swagger';
-setupSwagger(app);
 
-
+// Initialize express app first
 const app = express();
 
-// Middleware
-
+// Then setup middleware
 app.use(cors());
 app.use(express.json());
 
-// Routes
+// Setup routes
 app.use('/api/tasks', taskRoutes);
 
 // Connect to MongoDB
 connectDB();
 
+// Finally setup Swagger (now that app is properly initialized)
+setupSwagger(app);
+
 export default app;
+
+
+
+
+
+
+
+// import express from 'express',
+// import cors from 'cors';
+// import connectDB from './utils/db';
+// import taskRoutes from './routes/tasks';
+// import { setupSwagger } from './utils/swagger';
+// setupSwagger(app);
+
+
+// const app = express();
+
+// // Middleware
+
+// app.use(cors());
+// app.use(express.json());
+
+// // Routes
+// app.use('/api/tasks', taskRoutes);
+
+// // Connect to MongoDB
+// connectDB();
+
+// export default app;
