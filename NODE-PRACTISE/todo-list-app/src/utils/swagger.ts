fbec95b0754a5fc311304a8b4a84f363d5073066
Kariuki11 <irungukenneth433@gmail.com>
2025-03-24 13:11:58 +0300

    add swagger setup for API documentation in todo-list-app

---
+++ NODE-PRACTISE/todo-list-app/src/utils/swagger.ts
@@ -0,0 +1,21 @@
+import swaggerUi from 'swagger-ui-express';
+import { Express } from 'express';
+import swaggerJsdoc from 'swagger-jsdoc';
+
+const options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'To-Do List API',
+      version: '1.0.0',
+      description: 'API for managing tasks',
+    },
+  },
+  apis: ['./src/routes/*.ts'],
+};
+
+const specs = swaggerJsdoc(options);
+
+export const setupSwagger = (app: Express) => {
+  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
+};
