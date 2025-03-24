64b7eec5259139bebe3d9cd3449f787c3c66ffdf
Kariuki11 <irungukenneth433@gmail.com>
2025-03-24 13:06:01 +0300

    add tests for task creation and retrieval in todo-list-app

--- NODE-PRACTISE/todo-list-app/test/tasks.tests.ts
+++ NODE-PRACTISE/todo-list-app/test/tasks.tests.ts
@@ -1,21 +1,70 @@
 import chai from 'chai';
 import chaiHttp from 'chai-http';
 import app from '../src/app';
-import { Task } from '../src/models/task';
+import Task from '../src/models/Task';
 
 chai.use(chaiHttp);
+const expect = chai.expect;
 
 describe('Task API', () => {
-    beforeEach(async () => {
-        await Task.deleteMany({});
-    });
+  beforeEach(async () => {
+    await Task.deleteMany({});
+  });
 
-    it ('should create a new task', async () => {
-        const res = await chai
-            .request(app)
-            .post('/api/tasks')
-            .send({ title: 'Test Task', description: 'Test Description' });
-        expect(res.status).to.equal(201);
-        expect(res.body).to.have.property('_id');
-    });
-})
+  it('should create a new task', async () => {
+    const res = await chai
+      .request(app)
+      .post('/api/tasks')
+      .send({ title: 'Test Task', description: 'Test Description' });
+    expect(res.status).to.equal(201);
+    expect(res.body).to.have.property('_id');
+  });
+
+  it('should get all tasks', async () => {
+    await Task.create({ title: 'Task 1', description: 'Description 1' });
+    const res = await chai.request(app).get('/api/tasks');
+    expect(res.status).to.equal(200);
+    expect(res.body.length).to.equal(1);
+  });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import chai, { expect } from 'chai';
+// import chaiHttp from 'chai-http';
+// import app from '../src/app';
+// import { Task } from '../src/models/task';
+
+// chai.use(chaiHttp);
+
+// describe('Task API', () => {
+//     beforeEach(async () => {
+//         await Task.deleteMany({});
+//     });
+
+//     it ('should create a new task', async () => {
+//         const res = await chai
+//             .request(app)
+//             .post('/api/tasks')
+//             .send({ title: 'Test Task', description: 'Test Description' });
+//         expect(res.status).to.equal(201);
+//         expect(res.body).to.have.property('_id');
+//     });
+
+//     it('Should get all tasks', async () => {
+//         await Task.create({ title: 'Task1', description: 'Description1' });
+//         const res = await chai.request(app).get('/api/tasks');
+//         expect(res.status).to.equal(200);
+//         expect(res.body.length).to.equal(1);
+//     });
+// });
